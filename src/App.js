80d4a667f03a95093000fe60cfc82679d9ac0562
bellex0 <bellepx3@gmail.com>
2020-05-16 03:14:26 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {gql, useQuery} from '@apollo/client' 
+
+const query = gql`
+query {
+  popular_artists(size: 3) {
+    artists {
+      name
+      	artworks {
+      	  id
+          title
+          price
+          	image {
+              image_url
+              
+          	}
+      	}
+    }
+  }
+}
+`
 
 function App() {
+  const {loading, data} = useQuery(query)
+  if (loading) return <p>Loading....</p>
+console.log(data.popular_artists.artists[0].name)
+console.log(data.popular_artists.artists[0].artworks.map(work => work.image))
+console.log()
+
+const pablo = data.popular_artists.artists[0].name
+const banksy = data.popular_artists.artists[1].name
+const andy = data.popular_artists.artists[2].name
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section className="App">
+    <h1>Arts for Sale</h1>
+    <div id="pablo">
+    <h3>{pablo}</h3>
+    {data.popular_artists.artists[0].artworks.map(work => (
+      <>
+      <h2>{work.title}</h2>
+      <h3>{work.price}</h3>
+      </>
+    ))}
     </div>
+
+    </section>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {ApolloClient, HttpLink, InMemoryCache, ApolloProvider} from "@apollo/client"
+
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://metaphysics-staging.artsy.net/"
+  }),
+  cache: new InMemoryCache()
+})
+
+console.log(client)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+    </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
